0fb9a31afe52476a1539e84d1c8030fff2e54b37
Jack Jansonius <j.jansonius@home.nl>
2019-12-01 14:09:58 +0100

    final version producthunt

---
+++ producthunt/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import operator
+
+# let op: wordt niet gebruikt. Home-page bevindt zich in products!!
+# def homepage(request):
+#     return render(request, 'home.html')
+
